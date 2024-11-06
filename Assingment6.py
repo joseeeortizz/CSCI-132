703c19d44438336e895c5195aa82f817e5be30f6
Jose <joseortizv.nyc@gmail.com>
2024-11-06 13:02:12 -0500

    Assignment5.py & Assignment6.py ADD

---
+++ Assingment6.py
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Usage : Assignment4.py
+# Author : Jose Ortiz
+# Created on : November 5, 2024
+# Description : Assignment4.py
+#
+#*************************************************************
